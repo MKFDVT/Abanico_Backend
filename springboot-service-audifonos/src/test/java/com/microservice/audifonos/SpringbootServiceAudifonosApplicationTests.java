e0941429d71066f5a86e01ed558bfc4eb61367ff
MKFDVT <104604293+MKFDVT@users.noreply.github.com>
2023-10-30 17:23:22 -0600

    Add files via upload
    parte 3  de la tarea 3

---
+++ springboot-service-audifonos/src/test/java/com/microservice/audifonos/SpringbootServiceAudifonosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.audifonos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceAudifonosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
