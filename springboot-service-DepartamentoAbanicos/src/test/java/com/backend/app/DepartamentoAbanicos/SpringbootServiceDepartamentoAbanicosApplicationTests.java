c77d2c62bb20ad337cbd4ae27d41f51d221a12a2
MKFDVT <104604293+MKFDVT@users.noreply.github.com>
2023-11-01 19:34:09 -0600

    Parte 5 de la tarea 3
    Parte 5 de la tarea 3

---
+++ springboot-service-DepartamentoAbanicos/src/test/java/com/backend/app/DepartamentoAbanicos/SpringbootServiceDepartamentoAbanicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.app.DepartamentoAbanicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceDepartamentoAbanicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
