8907bf6f14aa6c8f11a0a50c6f5fda7f9482e879
MKFDVT <104604293+MKFDVT@users.noreply.github.com>
2023-11-29 19:28:27 -0600

    Tarea 5 y 6 parte 6
    Tarea 5 y 6 parte 6

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.microservice.usuarios.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.repository.query.Param;
+
+import com.microservice.entity.Usuario;
+
+@RepositoryRestResource(path="usuarios")
+public interface UsuarioRepository extends PagingAndSortingRepository <Usuario, Long>{
+	@RestResource(path="buscar-username")
+	public Usuario findByUsername(@Param("nombre") String username);
+	public Usuario findByUsernameAndEmail(String username,String email);
+
+}
