64bd39d4e942a3cc849f5d7cc12eedc03b5ebbe0
MKFDVT <104604293+MKFDVT@users.noreply.github.com>
2023-11-29 19:58:04 -0600

    Add files via upload

---
+++ springboot-service-usuarios-comunes/src/test/java/com/microservice/usuarios/comunes/SpringbootServiceUsuariosComunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.usuarios.comunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceUsuariosComunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
